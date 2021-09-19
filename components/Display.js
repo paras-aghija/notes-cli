65ceaa0171e450e291792949ea69759ecde04961
paras-aghija <parasaghija2000@gmail.com>
2021-09-20 03:35:43 +0530

    table format display

--- components/Display.js
+++ components/Display.js
@@ -1,23 +1,14 @@
 const React = require("react");
 const { Box, Text, Newline } = require("ink");
 const { getNotes } = require("../services");
+const Table = require("ink-table").default;
 
 const Display = ({ important = false }) => {
 	const [notes, setNotes] = React.useState([]);
 	React.useEffect(() => {
 		getNotes().then((n) => setNotes(n));
 	}, []);
-	return (
-		<Box flexDirection="row">
-			{notes.map((note) => (
-				<Box key={note.id} flexDirection="column" borderStyle="single">
-					<Text>Content: {note.content}</Text>
-					<Text>Date: {note.date}</Text>
-					<Text>Important: {note.important ? "yes" : "no"}</Text>
-				</Box>
-			))}
-		</Box>
-	);
+	return <Table data={notes} />;
 };
 
 module.exports = Display;
